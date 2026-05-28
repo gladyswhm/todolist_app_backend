dec6f0ef46538793014d0689bf29838ef4028be3
Gladys Woo Hui Min <gladyswoo@192.168.1.8>
2026-05-28 17:21:19 +0800

    Update Routes to Include IDs to Map API Calls Accurately

--- routes/api.php
+++ routes/api.php
@@ -9,13 +9,13 @@ use App\Http\Controllers\todos_controller;
 //to do list route
 Route::get('/todo-lists', [todolist_controller::class, 'index']);
 Route::post('/todo-lists', [todolist_controller::class, 'store']);
-Route::get('/todo-lists', [todolist_controller::class, 'show']);
-Route::put('/todo-lists', [todolist_controller::class, 'update']);
-Route::delete('/todo-lists', [todolist_controller::class, 'destroy']);
+Route::get('/todo-lists/{id}', [todolist_controller::class, 'show']);
+Route::put('/todo-lists/{id}', [todolist_controller::class, 'update']);
+Route::delete('/todo-lists/{id}', [todolist_controller::class, 'destroy']);
 
 //to do items route
 Route::get('/todos', [todos_controller::class, 'index']);
 Route::post('/todos', [todos_controller::class, 'store']);
-Route::get('/todos', [todos_controller::class, 'show']);
-Route::put('/todos', [todos_controller::class, 'update']);
-Route::delete('/todos', [todos_controller::class, 'destroy']);
+Route::get('/todos/{id}', [todos_controller::class, 'show']);
+Route::put('/todos/{id}', [todos_controller::class, 'update']);
+Route::delete('/todos/{id}', [todos_controller::class, 'destroy']);
